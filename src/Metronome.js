3010c71f7627ff7009388ebda2501ffc5883adea
Nathan <nathankirley@gmail.com>
2020-07-08 14:57:53 +0100

    sound works

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+// import duckAccent from './DuckAccent.mp3';
+// import duckBeat from './DuckBeat.mp3';
+import sound from './sound.js';
 
 class Metronome extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            on: false,
+            on: true,
             count: 0,
             bpm: 120,
             beatsPerMeasure: 4
         }
+
+        // this.duckAccent = new Audio(duckAccent);
+        // this.duckAccent = new Audio("./DuckAccent.mp3");
+        // this.duckBeat = new Audio(duckBeat);
+
+        //const accent = new UIfx({asset: duckAccent});
     }
 
     handleBpmChange = event => {
@@ -17,8 +26,21 @@ class Metronome extends Component {
         this.setState({bpm});
     }
 
+    // sound = () => {
+    //     const play = useSound(duckAccent);
+    // }
+
+    startStop = () => {
+        // console.log(this.duckAccent);
+        sound.play();
+    }
+
     render() {
         const {on, bpm} = this.state;
+
+        // const noise = new UIfx(
+        //     {asset: duckAccent}
+        // )
 
         return (
             <div className="metronome">
@@ -32,9 +54,9 @@ class Metronome extends Component {
                     onChange={this.handleBpmChange}
                     />
                 </div>
-            <button>
-                {on ? 'Stop' : 'Start'}
-            </button>
+                <button onClick={this.startStop}>
+                    {on ? 'Stop' : 'Start'}
+                </button>
             </div>
         );
     }
